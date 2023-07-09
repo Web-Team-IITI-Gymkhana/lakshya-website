437bb6055eafc1ef0e6a33d075c2871ae161fc9c
riddhi2765 <riddhichandekar2108@gmail.com>
2023-07-09 16:02:54 +0530

    slider added bg image changed

--- js/navbar.js
+++ js/navbar.js
@@ -5,3 +5,17 @@ $(window).on('scroll', function () {
         $('nav').addClass('sticky-nav');
     }
 });
+
+
+function navigate(event){
+    //get active element
+    var active = event.target.closest(".slide-container").getElementsByClassName("active")[0];
+    //remove class "active" from active element
+    if(active)active.classList.remove("active");
+    //get target element from link
+    var target = document.getElementById(event.target("a").getAttribute("href").replace("#",""));
+    //add class "active" to the target
+    target.classList.add("active");
+    event.preventDefault();
+    //prevent default link behaviour
+  }
