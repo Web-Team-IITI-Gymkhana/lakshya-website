302738034549ca1efde4cee9bf6ebba4340b312c
jayant <jayantbelwanshi1401>
2024-01-11 23:01:16 +0530

    fonts

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -74,7 +74,7 @@ const Grid = () => {
                  Unleash the power of Sports
             </div>
             
-            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4    mx-auto  ">
+            <div className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5    mx-auto  ">
                 {Images.map((data) => (
             <div className="mx-auto "><a href={data.eventurl} target="_blank"><Gridcard value={data}/></a></div>
                 ))}
