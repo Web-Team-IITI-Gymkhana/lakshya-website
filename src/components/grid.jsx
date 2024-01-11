c858199725b071ba0beced6f156774f3f30a98e8
jayant <jayantbelwanshi1401>
2024-01-12 00:47:55 +0530

    bugs fixed

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -70,9 +70,9 @@ const Grid = () => {
         
     ];
     return (
-        <div className="w-[90%] lg:w-[80%] bg-[#f7f7f7] mx-auto p-5 rounded-md">
-            <div className=" text-3xl font-bold p-7 text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center">
-                 Unleash the power of Sports
+        <div className="w-[90%] lg:w-[80%] bg-[#f7f7f7] mx-auto p-5 rounded-md ">
+            <div className=" text-3xl font-bold p-7   text-center">
+                 Unleash the Power of Sports
             </div>
             
             <div className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5    mx-auto  ">
